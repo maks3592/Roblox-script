2110dfc41aaeafb31f975ac899a2dcc481faf93c
maks3592 <tabackovmaks@gmail.com>
2025-06-02 21:12:40 +0300

    Create shortscript.lua

---
+++ shortscript.lua
@@ -0,0 +1,4 @@
+-- By @maks3592 from GitHub. Thanks for using :)
+-- Use at your own risk.
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/maks3592/repository/main/script.lua", true))()
