d525ba34f76e2866d1ae41105730d11a424f832e
maks3592 <tabackovmaks@gmail.com>
2025-06-02 20:32:47 +0300

    Добавлен HTML, CSS и видео

--- blackhole.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("mousemove", (event) => {
-    let blackhole = document.getElementById("blackhole");
-
-    let x = event.clientX - blackhole.offsetWidth / 2;
-    let y = event.clientY - blackhole.offsetHeight / 2;
-
-    blackhole.style.transform = `translate(${x}px, ${y}px)`;
-});
